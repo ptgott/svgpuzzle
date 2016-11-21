17475a24c1fb0f632648dfada47be3f41a99f38e
Paul Gottschling <paul.gottschling@gmail.com>
2016-11-21 13:37:14 +0000

    Edges are added around the perimeter of the grid

--- svg_puzzle.js
+++ svg_puzzle.js
@@ -1,6 +1,46 @@
+var allEdges = [];
+
+function createEdgesFromGridPerimeter(){
+  for(var i = 0; i < window.puzzleGrid.squaresX; i++){
+    new Edge(
+      new Point(i,0),
+      new Point(i+1,0)
+    );
+    new Edge(
+      new Point(i, window.puzzleGrid.squaresY),
+      new Point(i+1,window.puzzleGrid.squaresY)
+    );
+  }
+  
+  for(var j = 0; j < window.puzzleGrid.squaresY; j++){
+    new Edge(
+      new Point(0,j),
+      new Point(0, j+1)
+    );
+    new Edge(
+      new Point(window.puzzleGrid.squaresX, j),
+      new Point(window.puzzleGrid.squaresX, j+1)
+    );
+  }
+
+}
+
+function createEdgesFromGridSlices(countX, countY){
+}
+
 function Edge(point1, point2){
+  allEdges.push(this);
   this.point1 = point1;
   this.point2 = point2;
+  this.render = function(){
+    var line = window.puzzleSpace.line(
+      this.point1.x * window.puzzleGrid.squareSize, 
+      this.point1.y * window.puzzleGrid.squareSize, 
+      this.point2.x * window.puzzleGrid.squareSize, 
+      this.point2.y * window.puzzleGrid.squareSize
+    );
+    line.stroke({ width: '1px', color: 'black' });
+  }
 }
 
 function Grid(squaresX, squaresY, squareSizePx){
@@ -39,7 +79,16 @@ function Grid(squaresX, squaresY, squareSizePx){
   }  
 }
 
-function GridSlice(){
+function GridSliceAgent(axis){
+  // this.startValue = where it'll start on the axis
+  
+  // var currentEdge = an Edge object, the one last created
+  
+  // This function, based on the Points within the currentEdge, randomly
+  // determines whether to choose for the next currentEdge a vertical shift, a 45 degree
+  // angle or a horizontal shift, and in what direction.
+  // determineNextEdge = function(){
+  // }
   // this.axis = ; //define this: left to right or top to bottom?
 //   this.startPoint = ;//define this as a new Point
 }
@@ -59,5 +108,11 @@ window.onload = function(){
   window.puzzleGrid = new Grid(50, 50, 7);  
   window.puzzleSpace = SVG('puzzleSpace').size('100%', '100%');
   window.puzzleGrid.render();
+  createEdgesFromGridPerimeter();
+  createEdgesFromGridSlices(1, 0);
 
+  for(var e = 0; e < allEdges.length; e++){
+    allEdges[e].render();
+  }
+  
 }
